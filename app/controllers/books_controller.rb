84518e32040cc23a36dfed4ec892ce24d7dda12a
yoshikaneichimura <yoshikanemobile@gmail.com>
2026-04-27 13:29:32 +0000

    [Add] books_controller error_message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book_new = Book.new(book_params)
-    book_new.user_id = current_user.id
-    book_new.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to books_path
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+       flash[:notice] = "You have created book successfully."
+       redirect_to books_path
+    else
+       @books = Book.all
+       render :index
+    end
   end
 
   def show
@@ -22,10 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+    else
+        render :edit
+    end
   end
 
   def destroy
